0a5d6bdffebfee6d0060a1f613615239d8acff12
yuliyayevtukh <65871043+yuliyayevtukh@users.noreply.github.com>
2020-05-31 17:01:45 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slideer').slick({
+        autoplay:true,
+        autoplaySpeed:7000,
+		arrows:true,
+		dots:false,
+        pauseOnHover: false,
+		slidesToShow:1,
+		speed:350,
+	});
+});
